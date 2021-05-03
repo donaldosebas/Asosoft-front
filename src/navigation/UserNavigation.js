53ee9d0cdda251ade4de0f0cfc1b77222c7c9761
unknown <gar19683@uvg.edu.gt>
2021-05-03 11:02:57 -0600

    feat: navigation drawer for user navigation

--- src/navigation/UserNavigation.js
+++ src/navigation/UserNavigation.js
@@ -1,28 +1,41 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 import Home from '../views/home'
 import Sport from '../views/Sport'
 import playerProfile from '../views/playerProfile'
 
 const Stack = createStackNavigator()
 
+const UserNavigationStack = () => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="Home"
+      component={Home}
+    />
+    <Stack.Screen
+      name="Sport"
+      component={Sport}
+    />
+    <Stack.Screen
+      name="Player"
+      component={playerProfile}
+    />
+  </Stack.Navigator>
+)
+
+const Drawer = createDrawerNavigator()
+
+const UserNavigationDrawer = () => (
+  <Drawer.Navigator>
+    <Drawer.Screen name="Home" component={UserNavigationStack} />
+  </Drawer.Navigator>
+)
+
 const UserNavigation = () => (
   <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Home"
-        component={Home}
-      />
-      <Stack.Screen
-        name="Sport"
-        component={Sport}
-      />
-      <Stack.Screen
-        name="Player"
-        component={playerProfile}
-      />
-    </Stack.Navigator>
+    <UserNavigationDrawer />
   </NavigationContainer>
 )
 export default UserNavigation
