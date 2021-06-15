77b2b6598930675cf484362fd60a0e5838c720e0
Julio Herrera <jurhs2000@gmail.com>
2021-06-14 18:10:51 -0600

    feat: custom input for username and password

---
+++ src/components/textInput.js
@@ -0,0 +1,151 @@
+import React, { useState, useRef, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import {
+  StyleSheet, View, TextInput,
+} from 'react-native'
+// import IconIonic from 'react-native-vector-icons/Ionicons'
+import IconFeather from 'react-native-vector-icons/Feather'
+import Animated, { Easing } from 'react-native-reanimated'
+
+const PRIMARYCOLOR = '#1B9CC4'
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 5,
+    minWidth: 150,
+    width: 250,
+  },
+  title: {
+    position: 'absolute',
+    color: 'black',
+  },
+  none: {
+    color: PRIMARYCOLOR,
+    transform: [{ translateX: 5 }, { translateY: -25 }],
+  },
+  box: {
+    backgroundColor: 'white',
+    height: 42,
+    flexDirection: 'row',
+    borderWidth: 2,
+    borderColor: PRIMARYCOLOR,
+    borderRadius: 5,
+    marginTop: 20,
+    paddingTop: 5,
+    paddingBottom: 5,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+  },
+  icon: {
+    color: PRIMARYCOLOR,
+    transform: [{ scale: 2 }, { translateY: 7 }, { translateX: 8 }],
+  },
+  input: {
+    color: 'black',
+    marginLeft: 30,
+    top: -5,
+    width: '80%',
+    height: 40,
+  },
+  isFocused: {
+    color: PRIMARYCOLOR,
+  },
+})
+
+const animations = StyleSheet.create({
+  title: (focusAnim, value) => ({
+    transform: [
+      {
+        translateX: focusAnim.interpolate({
+          inputRange: (value === '') ? [0, 1] : [1, 1],
+          outputRange: [42, 5],
+        }),
+      },
+      {
+        translateY: focusAnim.interpolate({
+          inputRange: (value === '') ? [0, 1] : [1, 1],
+          outputRange: [9, -25],
+        }),
+      },
+    ],
+  }),
+})
+
+const Types = {
+  USERNAME: 'username',
+  PASSWORD: 'password',
+}
+
+const CustomTextInput = ({
+  title, value, onChangeText, type,
+}) => {
+  const [isFocused, setIsFocused] = useState(false)
+  const focusAnim = useRef(new Animated.Value(0)).current
+
+  useEffect(() => {
+    Animated.timing(focusAnim, {
+      toValue: isFocused ? 1 : 0,
+      duration: 100,
+      easing: Easing.bezier(0.4, 0, 0.2, 1),
+      useNativeDriver: true,
+    }).start()
+  }, [focusAnim, isFocused])
+
+  return (
+    <View style={styles.container}>
+      <View
+        style={styles.box}
+      >
+        <Animated.Text
+          style={[
+            styles.title,
+            isFocused ? styles.isFocused : {},
+            animations.title(focusAnim, value),
+            (value !== '') ? styles.none : {},
+          ]}
+        >
+          {title}
+        </Animated.Text>
+        {
+          (() => {
+            if (type === Types.USERNAME) return <IconFeather style={styles.icon} name="user" />
+            if (type === Types.PASSWORD) return <IconFeather style={styles.icon} name="lock" />
+            return ''
+          })()
+        }
+        <TextInput
+          style={[styles.input, isFocused ? styles.isFocused : {}]}
+          onChangeText={onChangeText}
+          value={value}
+          secureTextEntry={type === Types.PASSWORD}
+          onBlur={(event) => {
+            setIsFocused(false)
+            // onBlur?.(event)
+          }}
+          onFocus={(event) => {
+            setIsFocused(true)
+            // onFocus?.(event)
+          }}
+        />
+      </View>
+    </View>
+  )
+}
+
+CustomTextInput.propTypes = {
+  title: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChangeText: PropTypes.func.isRequired,
+  type: PropTypes.string.isRequired,
+}
+
+export {
+  CustomTextInput,
+  Types,
+}
--- src/views/Login.js
+++ src/views/Login.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { CustomTextInput, Types } from '../components/textInput'
 import { loginText } from '../text/es.json'
 
 const styles = StyleSheet.create({
@@ -7,12 +8,35 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'white',
   },
 })
 
-const Login = () => (
-  <View style={styles.container}>
-    <Text>{loginText.loginTitle}</Text>
-  </View>
-)
+const Login = () => {
+  const [user, setUser] = useState({
+    username: '',
+    password: '',
+  })
+
+  return (
+    <View style={styles.container}>
+      <Text>{loginText.loginTitle}</Text>
+      <CustomTextInput
+        title={loginText.usernameInput}
+        name="username"
+        value={user.username}
+        onChangeText={(newValue) => setUser((old) => ({ ...old, username: newValue }))}
+        type={Types.USERNAME}
+      />
+      <CustomTextInput
+        title={loginText.passwordInput}
+        name="password"
+        value={user.password}
+        onChangeText={(newValue) => setUser((old) => ({ ...old, password: newValue }))}
+        type={Types.PASSWORD}
+      />
+    </View>
+  )
+}
+
 export default Login
