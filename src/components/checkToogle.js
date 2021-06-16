929135131cae2c6295deebdc2bbb51f6f4ca21c5
Julio Herrera <jurhs2000@gmail.com>
2021-06-16 06:49:12 -0600

    feat: toggle remember me animation bounce

--- src/components/checkToogle.js
+++ src/components/checkToogle.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import PropTypes from 'prop-types'
-import { StyleSheet, View } from 'react-native'
+import { Animated, Easing, StyleSheet } from 'react-native'
 import IconIonic from 'react-native-vector-icons/Ionicons'
 
 const styles = StyleSheet.create({
@@ -18,14 +18,53 @@ const styles = StyleSheet.create({
   },
 })
 
-const CheckToogle = ({ toogled }) => (
-  <View style={styles.container}>
-    <IconIonic
-      name="checkmark-circle-outline"
-      style={[styles.icon, toogled ? styles.selected : styles.deselected]}
-    />
-  </View>
-)
+const animations = StyleSheet.create({
+  bounce: (animation) => ({
+    transform: [
+      {
+        scale: animation.interpolate({
+          inputRange: [0, 1],
+          outputRange: [1, 1.3],
+        }),
+      },
+    ],
+  }),
+})
+
+const CheckToogle = ({ toogled }) => {
+  const toogleAnim = useRef(new Animated.Value(0)).current
+
+  useEffect(() => {
+    Animated.sequence([
+      Animated.timing(toogleAnim, {
+        toValue: 1,
+        duration: 100,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      }),
+      Animated.timing(toogleAnim, {
+        toValue: 0,
+        duration: 100,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      }),
+    ]).start()
+  }, [toogleAnim, toogled])
+
+  return (
+    <Animated.View
+      style={[
+        styles.container,
+        animations.bounce(toogleAnim),
+      ]}
+    >
+      <IconIonic
+        name="checkmark-circle-outline"
+        style={[styles.icon, toogled ? styles.selected : styles.deselected]}
+      />
+    </Animated.View>
+  )
+}
 
 CheckToogle.propTypes = {
   toogled: PropTypes.bool.isRequired,
